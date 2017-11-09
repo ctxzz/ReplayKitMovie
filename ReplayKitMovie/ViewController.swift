9da433cec6b49e96aaf9f8b0e8490d6a8a1d436a
ctxzz <ctxs19@gmail.com>
2017-11-09 16:20:27 +0900

    project init

---
+++ ReplayKitMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReplayKitMovie
+//
+//  Created by Atsushi OMATA on 2017/11/09.
+//  Copyright © 2017 Atsushi OMATA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
